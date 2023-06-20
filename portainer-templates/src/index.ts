2d201baaf1ffeb2457f2ad7f1c3aff62223c15d0
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2023-06-20 10:10:09 +0200

    Better error handling + new source json

--- portainer-templates/src/index.ts
+++ portainer-templates/src/index.ts
@@ -12,20 +12,23 @@ let cachedResponse = {
 async function handleRequest(request: any) {
 
   const extraSources = [
+    'https://raw.githubusercontent.com/xneo1/portainer_templates/master/Template/template.json',
     'https://raw.githubusercontent.com/mycroftwilde/portainer_templates/master/Template/template.json',
     'https://raw.githubusercontent.com/technorabilia/portainer-templates/main/lsio/templates/templates-2.0.json',
     'https://raw.githubusercontent.com/mikestraney/portainer-templates/master/templates.json',
-    'https://raw.githubusercontent.com/donPabloNow/selfhosted-saas/master/Template/portainer-v2.json',
     'https://raw.githubusercontent.com/dnburgess/self-hosted-template/master/template.json',
     'https://raw.githubusercontent.com/Qballjos/portainer_templates/master/Template/template.json',
     'https://raw.githubusercontent.com/SelfhostedPro/selfhosted_templates/portainer-2.0/Template/template.json',
     'https://raw.githubusercontent.com/OliverCullimore/portainer-templates/master/templates.json',
     'https://raw.githubusercontent.com/mediadepot/templates/master/portainer.json',
-    'https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json'
+    'https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json',
+    'https://raw.githubusercontent.com/ntv-one/portainer/main/template.json'
   ];
 
-  const base = await fetch('https://raw.githubusercontent.com/xneo1/portainer_templates/master/Template/template.json');
-  const baseJson = await base.json() as any;
+  const baseJson = {
+    version: "2",
+    templates: []
+  }
 
   if (cachedResponse.response && cachedResponse.cacheTime > Date.now() - 1000 * 60 * 60) {
     console.log('Cached!')
@@ -37,14 +40,18 @@ async function handleRequest(request: any) {
   }
 
   const promises = extraSources.map(async (source) => {
-    const response = await fetch(source);
-    const json = await response.json() as any;
-    json.templates.forEach((template: any) => {
-      //check if template.image already exists in baseJson
-      if (!baseJson.templates.find((t: any) => t.image === template.image)) {
-        baseJson.templates.push(template);
-      }
-    });
+    try {
+      const response = await fetch(source);
+      const json = await response.json() as any;
+      json.templates.forEach((template: any) => {
+        //check if template.image already exists in baseJson
+        if (!baseJson.templates.find((t: any) => t.image === template.image)) {
+          baseJson.templates.push(template);
+        }
+      });
+    } catch (e) {
+      console.log('Error fetching ' + source);
+    }
   });
   await Promise.all(promises);
 
